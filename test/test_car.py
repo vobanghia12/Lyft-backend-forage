3f005a14a3f49a83f153f60cceff4853ea00c6ee
Nghia Vo <vobanghia12@gmail.com>
2022-08-17 19:14:35 -0400

    fix some erros

--- test/test_car.py
+++ test/test_car.py
@@ -1,13 +1,5 @@
 import unittest
 from datetime import datetime
-from ..engine.engine import Engine
-
-from engine.model.calliope import Calliope
-from engine.model.glissade import Glissade
-from engine.model.palindrome import Palindrome
-from engine.model.rorschach import Rorschach
-from engine.model.thovex import Thovex
-
 from CarFactory.carfactory import CarFactory
 from Car.car import Car
 
